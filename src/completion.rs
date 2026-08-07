7682c6ffdd1e011b4eab472c69184ce0bb6e36c7
Andres Eloy Rivera Garcia <andres.degozaru@gmail.com>
2026-08-07 20:39:33 +0200

    Add completions for characters command + improve function injection
    Closes #8

--- src/completion.rs
+++ src/completion.rs
@@ -1,5 +1,6 @@
 use clap::Command;
 use clap_complete::{generate, shells};
+use regex::RegexBuilder;
 
 const ANIME_INFO_ZSH_COMPLETION: &str = r#"
 _ani_info() {
@@ -55,14 +56,26 @@ pub fn generate_completions(cmd: &mut Command) -> Result<String, Box<dyn std::er
 
     generate(shells::Zsh, cmd, name, &mut buffer);
 
-    let zsh_completion = String::from_utf8(buffer)?
-        // TODO need to find a better way to connect the completion search functions
-        .replacen("id:_default", "id:_ani_info", 1)
-        .replacen(
-            "#compdef ani",
-            &format!("#compdef ani\n{ANIME_INFO_ZSH_COMPLETION}"),
-            1,
-        );
+    let info_function_regex = RegexBuilder::new(r"(?<start>\(info\).*?:id[^\n]*:)_default")
+        .dot_matches_new_line(true)
+        .multi_line(true)
+        .build()?;
+
+    let characters_function_regex =
+        RegexBuilder::new(r"(?<start>\(characters\).*?:id[^\n]*:)_default")
+            .dot_matches_new_line(true)
+            .multi_line(true)
+            .build()?;
+
+    let zsh_completion = String::from_utf8(buffer)?;
+    let zsh_completion = info_function_regex.replace(&zsh_completion, "${start}_ani_info");
+    let zsh_completion = characters_function_regex.replace(&zsh_completion, "${start}_ani_info");
+
+    let zsh_completion = zsh_completion.replacen(
+        "#compdef ani",
+        &format!("#compdef ani\n{ANIME_INFO_ZSH_COMPLETION}"),
+        1,
+    );
 
     Ok(zsh_completion)
 }
